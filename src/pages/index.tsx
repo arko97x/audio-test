10448c61f4edd17323b0c4c3126dd348f9f957ee
Arkoprabho Bhattacharjee <bhattacharjee.arkoprabho@gmail.com>
2023-05-19 06:52:52 +0530

    Tried to get Tone.js to crash through an AudioEngine overload

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,29 @@
+import * as Tone from "tone"
+import { useEffect } from "react"
+
 export default function Home() {
+
+  useEffect(() => {
+    const synth = new Tone.Synth().toDestination()
+
+    function handleKeyPress(event: { keyCode: number }) {
+      if (event.keyCode === 13) {
+        synth.triggerAttackRelease("G4", "8n")
+      }
+      synth.triggerAttackRelease("G4", "8n")
+    }
+
+    document.addEventListener('keypress', handleKeyPress)
+
+    return () => {
+      document.removeEventListener('keypress', handleKeyPress)
+    }
+  }, [])
+
   return (
     <main
       className={``}
     >
-      GGWP
     </main>
   )
 }
